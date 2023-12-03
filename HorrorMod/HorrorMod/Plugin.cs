e4dd2f3e622e9645e22484070ce772678b8bb847
striker67 <86896895+Striker-67@users.noreply.github.com>
2023-12-02 20:56:18 -0600

    tiny update

--- HorrorMod/HorrorMod/Plugin.cs
+++ HorrorMod/HorrorMod/Plugin.cs
@@ -7,11 +7,8 @@ using Utilla;
 
 namespace HorrorMod
 {
-    /// <summary>
-    /// This is your mod's main class.
-    /// </summary>
 
-    /* This attribute tells Utilla to look for [ModdedGameJoin] and [ModdedGameLeave] */
+
     [ModdedGamemode]
     [BepInDependency("org.legoandmars.gorillatag.utilla", "1.5.0")]
     [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
@@ -19,13 +16,14 @@ namespace HorrorMod
     public class Plugin : BaseUnityPlugin
     {
         GameObject assest;
+        GameObject portal;
         bool inRoom;
 
         void Start()
         {
-            /* A lot of Gorilla Tag systems will not be set up when start is called /*
-			/* Put code in OnGameInitialized to avoid null references */
 
+            // pos 132.4225 -15.5873 0
+            // rot 0 298.6074 0
             Utilla.Events.GameInitialized += OnGameInitialized;
         }
 
@@ -47,12 +45,22 @@ namespace HorrorMod
 
 
             }
+            //pos -68.6526 11.8042 -81.8469
+            // rot 0.1144 84.743 0
+            //scale -0.0422 0.9351 0.6931
+            portal = assest.transform.Find("Cube (2)").gameObject;
+
+            portal.transform.rotation = Quaternion.Euler(0.1144f, 263.7137f, 0f);
+            portal.transform.localScale = new Vector3(-0.1422f, 0.9351f, 0.5931f);
+            portal.transform.Find("Trigger for portal").gameObject.AddComponent<Portal>();
+
+            assest.transform.position = new Vector3(132.4225f, -15.5873f, 0f);
+            assest.transform.rotation = Quaternion.Euler(0f, 298.6074f, 0f);
+            portal.transform.localPosition = new Vector3(-68.9155f, 11.86f, -81.8204f);
+            portal.transform.position = new Vector3(-68.9155f, 11.86f, -81.8204f);
+            assest.SetActive(false);
         }
 
-        void Update()
-        {
-
-        }
         public AssetBundle LoadAssetBundle(string path)
         {
             Stream stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(path);
@@ -62,27 +70,26 @@ namespace HorrorMod
         }
 
 
-        /* This attribute tells Utilla to call this method when a modded room is joined */
+        
         [ModdedGamemodeJoin]
         public void OnJoin(string gamemode)
         {
             if (gamemode.Contains("HORROR"))
             {
                 Debug.Log("Joined");
-                RenderSettings.ambientSkyColor = Color.grey;
-
+                RenderSettings.ambientSkyColor = Color.black;
+                assest.SetActive(true);
                 inRoom = true;
             }
           
         }
 
-        /* This attribute tells Utilla to call this method when a modded room is left */
+       
         [ModdedGamemodeLeave]
         public void OnLeave(string gamemode)
         {
-            /* Deactivate your mod here */
-            /* This code will run regardless of if the mod is enabled*/
-
+            RenderSettings.ambientSkyColor = Color.white;
+            assest.SetActive(false);
             inRoom = false;
         }
     }
---
+++ HorrorMod/HorrorMod/Portal.cs
@@ -0,0 +1,34 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using GorillaLocomotion;
+using UnityEngine;
+
+namespace HorrorMod
+{
+    public class Portal : GorillaTriggerBox
+    {
+        // Code from devs minecraft mod: https://github.com/developer9998/DevMinecraftMod/blob/main/DevMinecraftMod/Scripts/MinecraftQuitBox.cs & DecalFree!
+        public void Start()
+        {
+            this.gameObject.layer = 15;
+        }
+        public override void OnBoxTriggered()
+        {
+            Vector3 target = new Vector3(111.5544f, - 33.9543f, - 25.0447f);
+
+            Traverse.Create(Player.Instance).Field("lastPosition").SetValue(target);
+            Traverse.Create(Player.Instance).Field("lastLeftHandPosition").SetValue(target);
+            Traverse.Create(Player.Instance).Field("lastRightHandPosition").SetValue(target);
+            Traverse.Create(Player.Instance).Field("lastHeadPosition").SetValue(target);
+
+            Player.Instance.leftControllerTransform.position = target;
+            Player.Instance.rightControllerTransform.position = target;
+            Player.Instance.bodyCollider.attachedRigidbody.transform.position = target;
+
+            Player.Instance.GetComponent<Rigidbody>().position = target;
+            Player.Instance.GetComponent<Rigidbody>().velocity = Vector3.zero;
+        }
+    }
+}
